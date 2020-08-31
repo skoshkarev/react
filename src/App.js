5043274f63d4d0df80593e8c55434d91117ce12d
Sergey Koshkarev <koshkarev.ss@gmail.com>
2020-08-31 20:42:12 +0000

    Changes Aug 31st

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import MoviesApp from 'redux';
 
-function App() {
+import logo from './logo.svg';
+import './css/index.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-	<h3>Hello World</h3>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+        <nav>
+          <ul>
+            <li>
+              <Link className="App-link" to="/">Home</Link>
+            </li>
+            <li>
+              <Link className="App-link" to="/movies">Movies</Link>
+            </li>
+          </ul>
+        </nav>
+        </header>
+        <Switch>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+          <Route path="/movies/:slug">
+            <Movie />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Movies() {
+  return <h2>Movies</h2>;
+}
+
+function Movie() {
+  return <h2>Selected Movie</h2>;
+}
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
